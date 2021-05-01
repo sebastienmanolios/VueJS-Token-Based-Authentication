bb05c4cec43f8d986532b05061753cb6f9c79712
sebastienmanolios <sebastien.manolios@gmail.com>
2021-05-01 07:11:31 +0200

    user-login

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -28,6 +28,19 @@ export default new Vuex.Store({
         .then(({ data }) => {
           commit('SET_USER_DATA', data)
         })
+    },
+    login ({ commit }, credentials) {
+      return axios
+        .post('//localhost:3000/login', credentials)
+        .then(({ data }) => {
+          commit('SET_USER_DATA', data)
+        })
+    }
+  },
+  getters: {
+    loggedIn (state) {
+      // return the truthiness or the falseness of the value
+      return !!state.user
     }
   }
 })
